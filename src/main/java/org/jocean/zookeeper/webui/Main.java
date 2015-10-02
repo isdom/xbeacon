2a1ef1e451962b3cce098c783b15da112317ca27
ming.ma <isdom.maming@gmail.com>
2015-10-02 21:03:23 +0800

    add jetty * lib dependencies; add demo hello.zul to run zkoss with jetty console mode

---
+++ src/main/java/org/jocean/zookeeper/webui/Main.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package org.jocean.zookeeper.webui;
+
+import java.io.File;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @author Marvin.Ma
+ *
+ */
+public class Main {
+
+    private static final Logger LOG = 
+            LoggerFactory.getLogger(Main.class);
+    
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+        final Server server = new Server(7080);
+        
+        final WebAppContext context = new WebAppContext();
+        context.setContextPath("/");
+
+        final String warfile = System.getProperty("user.dir") 
+                + System.getProperty("file.separator")
+                + "bin"
+                + System.getProperty("file.separator")
+                + System.getProperty("app.name", "jocean-zk-webui")
+                + ".jar"
+                ;
+        if ( new File(warfile).exists() ) {
+            LOG.info("found warfile {}, and use this file as war", warfile);
+            context.setWar(warfile);
+        }
+        else {
+            LOG.info("can't found warfile {}, try default web content info", warfile);
+            context.setDescriptor( "scripts/webcontent/WEB-INF/web.xml");
+            context.setResourceBase( "scripts/webcontent/");
+            context.setParentLoaderPriority(true);
+        }
+        
+        server.setHandler(context);
+ 
+        server.start();
+        server.join();
+	}
+
+}
