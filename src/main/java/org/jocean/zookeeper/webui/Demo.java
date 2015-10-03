65a3cee83f4e48e9a78aba78ecc4a951a863f45a
ming.ma <isdom.maming@gmail.com>
2015-10-03 22:34:43 +0800

    adjust log style; add Spring support

---
+++ src/main/java/org/jocean/zookeeper/webui/Demo.java
@@ -0,0 +1,12 @@
+package org.jocean.zookeeper.webui;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Demo {
+    private static final Logger LOG = 
+            LoggerFactory.getLogger(Demo.class);
+    public Demo() {
+        LOG.info("demo started...");
+    }
+}
