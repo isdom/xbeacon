d756ac860f20e5b771b67b21fdf6505a00916bf9
ming.ma <isdom.maming@gmail.com>
2015-10-07 21:01:22 +0800

    add WebApp fetch Hook

---
+++ src/main/java/org/jocean/zookeeper/webui/MyWebAppInit.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.jocean.zookeeper.webui;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.zkoss.zk.ui.WebApp;
+import org.zkoss.zk.ui.util.WebAppInit;
+
+/**
+ * @author isdom
+ *
+ */
+public class MyWebAppInit implements WebAppInit {
+
+    private static final Logger LOG = 
+            LoggerFactory.getLogger(MyWebAppInit.class);
+    
+	/* (non-Javadoc)
+	 * @see org.zkoss.zk.ui.util.WebAppInit#init(org.zkoss.zk.ui.WebApp)
+	 */
+	@Override
+	public void init(final WebApp webapp) throws Exception {
+	    LOG.info("webapp {} init.", webapp);
+	}
+
+}
