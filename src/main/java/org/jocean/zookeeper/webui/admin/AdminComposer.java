9cef29142a6e04788bdfbd0bdeb0db24ac1a1dcd
ming.ma <isdom.maming@gmail.com>
2015-10-14 22:25:32 +0800

    分离 NodeContent 为独立文件

--- src/main/java/org/jocean/zookeeper/webui/admin/AdminComposer.java
+++ src/main/java/org/jocean/zookeeper/webui/admin/AdminComposer.java
@@ -12,18 +12,14 @@ import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.EventListener;
 import org.zkoss.zk.ui.event.EventQueues;
 import org.zkoss.zk.ui.event.Events;
-import org.zkoss.zk.ui.event.InputEvent;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
 import org.zkoss.zul.Button;
 import org.zkoss.zul.Caption;
-import org.zkoss.zul.Menubar;
 import org.zkoss.zul.Menuitem;
 import org.zkoss.zul.Messagebox;
-import org.zkoss.zul.Tab;
-import org.zkoss.zul.Tabpanel;
 import org.zkoss.zul.Tabpanels;
 import org.zkoss.zul.Tabs;
 import org.zkoss.zul.Textbox;
@@ -36,12 +32,11 @@ import org.zkoss.zul.Window;
 
 import com.google.common.base.Charsets;
 
+import rx.functions.Action0;
+
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
 public class AdminComposer extends SelectorComposer<Window>{
 	
-    /**
-     * 
-     */
     private static final long serialVersionUID = -9063622647244777848L;
 
     private static final Logger LOG = 
@@ -66,7 +61,6 @@ public class AdminComposer extends SelectorComposer<Window>{
 		});
 		
         addnode.addEventListener(Events.ON_CLICK, new EventListener<Event>() {
-
             @Override
             public void onEvent(final Event event) throws Exception {
                 final SimpleTreeModel.Node node = currentSelectedNode();
@@ -77,7 +71,6 @@ public class AdminComposer extends SelectorComposer<Window>{
             }});
         
         delnode.addEventListener(Events.ON_CLICK, new EventListener<Event>() {
-
             @Override
             public void onEvent(final Event event) throws Exception {
                 final SimpleTreeModel.Node node = currentSelectedNode();
@@ -181,7 +174,12 @@ public class AdminComposer extends SelectorComposer<Window>{
             content.select();
         } else {
             this._contents.put(path, 
-                new NodeContent(node, path, this.maintabs, this.maintabpanels));
+                new NodeContent(this._zka, node, path, this.maintabs, this.maintabpanels,
+                        new Action0() {
+                            @Override
+                            public void call() {
+                                _contents.remove(path);
+                            }}));
         }
     }
 
@@ -219,8 +217,7 @@ public class AdminComposer extends SelectorComposer<Window>{
             item.appendChild( new Treerow() {
                 private static final long serialVersionUID = 1L;
             {
-                this.appendChild(
-                    new Treecell(node.getName()));
+                this.appendChild(new Treecell(node.getName()));
             }});
         }
     }
@@ -243,112 +240,9 @@ public class AdminComposer extends SelectorComposer<Window>{
     @Wire
     private Caption         status;
     
-    private class NodeContent {
-        private final Node _node;
-        private final Tab _tab;
-        private final Textbox _textbox;
-        private final Menuitem _save;
-        private boolean _isModified = false;
-        
-        NodeContent(final Node node, 
-                final String path, 
-                final Tabs tabs, 
-                final Tabpanels tabpanels) {
-            this._node = node;
-            this._tab = new Tab(path) {
-                void doClose() {
-                    super.close();
-                    _contents.remove(path);
-                }
-                
-                /* (non-Javadoc)
-                 * @see org.zkoss.zul.Tab#close()
-                 */
-                @Override
-                public void close() {
-                    if (isModified()) {
-                        Messagebox.show("Content has modified, Are you sure to discard?", "Confirm Dialog", 
-                                Messagebox.OK | Messagebox.CANCEL, 
-                                Messagebox.QUESTION, 
-                                new EventListener<Event>() {
-                            public void onEvent(Event evt) throws InterruptedException {
-                                if (evt.getName().equals("onOK")) {
-                                    doClose();
-                                }
-                            }});
-                    } else {
-                        doClose();
-                    }
-                }
-                private static final long serialVersionUID = 1L;
-                {
-                    this.setClosable(true);
-                }};
-            tabs.appendChild(this._tab);
-            this._textbox = new Textbox() {
-                private static final long serialVersionUID = 1L;
-            {
-                this.setWidth("100%");
-                this.setHeight("100%");
-                this.setMultiline(true);
-                this.setText(_zka.getNodeDataAsString(node));
-            }};
-            this._textbox.addEventListener(Events.ON_CHANGING, new EventListener<InputEvent>() {
-                @Override
-                public void onEvent(final InputEvent event) throws Exception {
-                    markModified();
-                }});
-            this._save = new Menuitem("save") {
-                private static final long serialVersionUID = 1L;
-            {
-                this.setDisabled(true);
-            }};
-            this._save.addEventListener(Events.ON_CLICK, new EventListener<Event>() {
-                @Override
-                public void onEvent(final Event event) throws Exception {
-                    saveContentToZK();
-                }});
-            tabpanels.appendChild(new Tabpanel() {
-                private static final long serialVersionUID = 1L; {
-                    this.appendChild(new Menubar() {
-                        private static final long serialVersionUID = 1L;
-                        {
-                            this.appendChild(_save);
-                        }
-                    });
-                    this.appendChild(_textbox);
-                }});
-            this._tab.setSelected(true);
-        }
-        
-        void select() {
-            this._tab.setSelected(true);
-        }
-
-        private boolean isModified() {
-            return this._isModified;
-        }
-        
-        private void markModified() {
-            if (!this._isModified) {
-                this._isModified = true;
-                this._tab.setLabel(_zka.getNodePath(this._node) + " *");
-                this._save.setDisabled(false);
-            }
-        }
-        
-        private void saveContentToZK() throws Exception {
-            _zka.setNodeDataAsString( this._node,  this._textbox.getText());
-            this._isModified = false;
-            this._tab.setLabel(_zka.getNodePath(this._node));
-            this._save.setDisabled(true);
-        }
-    }
-    
     private final Map<String, NodeContent>  _contents = new HashMap<>();
     
-	@WireVariable("zkagent")
-	private ZKAgent _zka;
+	@WireVariable("zkagent") ZKAgent _zka;
 	
     @WireVariable("rootPath")
     private String _rootPath;
