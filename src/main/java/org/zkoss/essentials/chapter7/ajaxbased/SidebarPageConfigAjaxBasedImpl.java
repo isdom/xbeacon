603b2c108cabdf451a449fd75115395f9cc4f7be
ming.ma <isdom.maming@gmail.com>
2017-04-28 09:36:02 +0800

    修改首页呈现模式

--- src/main/java/org/zkoss/essentials/chapter7/ajaxbased/SidebarPageConfigAjaxBasedImpl.java
+++ src/main/java/org/zkoss/essentials/chapter7/ajaxbased/SidebarPageConfigAjaxBasedImpl.java
@@ -19,6 +19,7 @@ public class SidebarPageConfigAjaxBasedImpl implements SidebarPageConfig{
 	
 	HashMap<String,SidebarPage> pageMap = new LinkedHashMap<String,SidebarPage>();
 	public SidebarPageConfigAjaxBasedImpl(){		
+		/*
 		pageMap.put("zk",new SidebarPage("zk","www.zkoss.org","/imgs/site.png","http://www.zkoss.org/"));
 		pageMap.put("demo",new SidebarPage("demo","ZK Demo","/imgs/demo.png","http://www.zkoss.org/zkdemo"));
 		pageMap.put("devref",new SidebarPage("devref","ZK Developer Reference","/imgs/doc.png","http://books.zkoss.org/wiki/ZK_Developer's_Reference"));
@@ -27,6 +28,9 @@ public class SidebarPageConfigAjaxBasedImpl implements SidebarPageConfig{
 		pageMap.put("fn2",new SidebarPage("fn2","Profile (MVVM)","/imgs/fn.png","/chapter5/profile-mvvm.zul"));
 		pageMap.put("fn3",new SidebarPage("fn3","Todo List (MVC)","/imgs/fn.png","/chapter6/todolist-mvc.zul"));
 		pageMap.put("fn4",new SidebarPage("fn4","Todo List (MVVM)","/imgs/fn.png","/chapter6/todolist-mvvm.zul"));
+		*/
+		pageMap.put("zk",new SidebarPage("zk","配置","/imgs/demo.png","/zkadmin/index.zul"));
+		pageMap.put("srv",new SidebarPage("srv","服务","/imgs/doc.png","/jconsole/index.zul"));
 	}
 	
 	public List<SidebarPage> getPages(){
