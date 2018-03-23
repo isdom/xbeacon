db337a0de9989d40159a0a3a320b7245c310f587
ming.ma <isdom.maming@gmail.com>
2018-03-23 18:18:43 +0800

    remove booter.*

---
+++ src/main/java/org/jocean/XBeaconMain.java
@@ -0,0 +1,11 @@
+package org.jocean;
+
+import org.jocean.j2se.booter.LocalMain;
+
+public class XBeaconMain {
+
+    public static void main(String[] args) throws Exception {
+        LocalMain.main(args);
+    }
+
+}
